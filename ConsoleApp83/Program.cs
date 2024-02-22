9e13ab2443a89eb3136eea6cf61b30410165f251
ygursu <yasemingrs@gmail.com>
2024-02-22 22:59:06 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp83/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedikdortgenlerprizmasiyuzeyalani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("a kenarını girin");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("b kenarını girin");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("c kenarını girin");
+            int c = Int32.Parse(Console.ReadLine());
+
+            int yuzeyalani = 2 * (a * b + a * c + b * c);
+            Console.WriteLine("Dikdörtgenler prizmasının yüzey alanı=" + yuzeyalani);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
